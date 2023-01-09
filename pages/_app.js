37bb42621086137237a7b9b31c21276a61b870ad
Maris <mcclapperton@gmail.com>
2023-01-08 21:26:17 -0700

    figuring out next

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+
+componentDidMount() {
+  fetch('http://www.omdbapi.com/?apikey=REACT_APP__APIKey&')
+}
+
+
   return <Component {...pageProps} />
 }
